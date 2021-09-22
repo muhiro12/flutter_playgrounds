bc45cf0e35047bb2e504b0ad2dfc1626e6340d1e
Hiromu Nakano <muhiro.12@gmail.com>
2021-09-22 21:52:02 +0900

    Enable ListPage to transition when bottom tab tapped

--- lib/presentation/page/long_list_page.dart
+++ lib/presentation/page/long_list_page.dart
@@ -1,14 +1,36 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
 
+// Package imports:
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
 // Project imports:
 import 'package:flutter_playgrounds/presentation/model/app_route.dart';
+import 'package:flutter_playgrounds/presentation/model/bottom_tab.dart';
+import 'package:flutter_playgrounds/presentation/model/bottom_tab_item.dart';
 
-class LongListPage extends StatelessWidget {
+class LongListPage extends HookConsumerWidget {
   const LongListPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final ScrollController scrollController = useScrollController();
+    ref.listen(
+      bottomTabProvider,
+      (BottomTab bottomTab) {
+        if (bottomTab.item != BottomTabItem.list ||
+            bottomTab.item != bottomTab.oldItem) {
+          return;
+        }
+        Navigator.of(context).popUntil((Route<dynamic> route) => route.isFirst);
+        scrollController.animateTo(
+          0,
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.linear,
+        );
+      },
+    );
     return Scaffold(
       appBar: AppBar(
         title: Text(toString()),
@@ -16,6 +38,7 @@ class LongListPage extends StatelessWidget {
       body: SafeArea(
         bottom: false,
         child: ListView.separated(
+          controller: scrollController,
           itemBuilder: (_, int index) => ListTile(
             onTap: () =>
                 Navigator.of(context).pushNamed(AppRoute.longList.name),
