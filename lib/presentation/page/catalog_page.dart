8599c9aea539bc48759bc0929d482c3dd8be64e7
muhiro_ <muhiro.12@gmail.com>
2021-09-27 23:03:28 +0900

    Make catalog page possible to back on tab tapped

--- lib/presentation/page/catalog_page.dart
+++ lib/presentation/page/catalog_page.dart
@@ -1,18 +1,32 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_playgrounds/presentation/model/app_route.dart';
+import 'package:flutter_playgrounds/presentation/model/bottom_tab.dart';
+import 'package:flutter_playgrounds/presentation/model/bottom_tab_item.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class CatalogPage extends StatelessWidget {
+class CatalogPage extends ConsumerWidget {
   const CatalogPage({Key? key}) : super(key: key);
 
+  List<AppRoute> get appRoutes => AppRoute.values
+      .where(
+        (AppRoute appRoute) =>
+            appRoute != AppRoute.catalog && appRoute.name.contains('catalog'),
+      )
+      .toList();
+
   @override
-  Widget build(BuildContext context) {
-    final List<AppRoute> appRoutes = AppRoute.values
-        .where(
-          (AppRoute appRoute) =>
-              appRoute != AppRoute.catalog && appRoute.name.contains('catalog'),
-        )
-        .toList();
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen(
+      bottomTabProvider,
+      (BottomTab bottomTab) {
+        if (bottomTab.item != BottomTabItem.catalog ||
+            bottomTab.item != bottomTab.oldItem) {
+          return;
+        }
+        Navigator.of(context).popUntil((Route<dynamic> route) => route.isFirst);
+      },
+    );
     return Scaffold(
       appBar: AppBar(
         title: Text(toString()),
