8db3fddcfd0c6bcd82b1976800f6d5bde5d7ea70
muhiro_ <muhiro.12@gmail.com>
2021-09-27 18:54:00 +0900

    Add material colors

--- lib/presentation/model/primary_color.dart
+++ lib/presentation/model/primary_color.dart
@@ -2,8 +2,24 @@ import 'package:flutter/material.dart';
 
 enum PrimaryColor {
   red,
+  pink,
+  purple,
+  deepPurple,
+  indigo,
   blue,
+  lightBlue,
+  cyan,
+  teal,
   green,
+  lightGreen,
+  lime,
+  yellow,
+  abmer,
+  orange,
+  deepOrange,
+  brown,
+  grey,
+  blueGrey,
 }
 
 extension PrimaryColorExtension on PrimaryColor {
@@ -11,10 +27,42 @@ extension PrimaryColorExtension on PrimaryColor {
     switch (this) {
       case PrimaryColor.red:
         return Colors.red;
+      case PrimaryColor.pink:
+        return Colors.pink;
+      case PrimaryColor.purple:
+        return Colors.purple;
+      case PrimaryColor.deepPurple:
+        return Colors.deepPurple;
+      case PrimaryColor.indigo:
+        return Colors.indigo;
       case PrimaryColor.blue:
         return Colors.blue;
+      case PrimaryColor.lightBlue:
+        return Colors.lightBlue;
+      case PrimaryColor.cyan:
+        return Colors.cyan;
+      case PrimaryColor.teal:
+        return Colors.teal;
       case PrimaryColor.green:
         return Colors.green;
+      case PrimaryColor.lightGreen:
+        return Colors.lightGreen;
+      case PrimaryColor.lime:
+        return Colors.lime;
+      case PrimaryColor.yellow:
+        return Colors.yellow;
+      case PrimaryColor.abmer:
+        return Colors.amber;
+      case PrimaryColor.orange:
+        return Colors.orange;
+      case PrimaryColor.deepOrange:
+        return Colors.deepOrange;
+      case PrimaryColor.brown:
+        return Colors.brown;
+      case PrimaryColor.grey:
+        return Colors.grey;
+      case PrimaryColor.blueGrey:
+        return Colors.blueGrey;
     }
   }
 }
