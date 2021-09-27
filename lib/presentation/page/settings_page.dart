1c022868437ad7528e4efce2273cd5c7e307aaa1
muhiro_ <muhiro.12@gmail.com>
2021-09-27 22:26:23 +0900

    Make settings page possible to back on tab tapped

--- lib/presentation/page/settings_page.dart
+++ lib/presentation/page/settings_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_playgrounds/business/model/primary.dart';
 import 'package:flutter_playgrounds/presentation/model/app_route.dart';
+import 'package:flutter_playgrounds/presentation/model/bottom_tab.dart';
+import 'package:flutter_playgrounds/presentation/model/bottom_tab_item.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class SettingsPage extends ConsumerWidget {
@@ -9,6 +11,16 @@ class SettingsPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen(
+      bottomTabProvider,
+      (BottomTab bottomTab) {
+        if (bottomTab.item != BottomTabItem.settings ||
+            bottomTab.item != bottomTab.oldItem) {
+          return;
+        }
+        Navigator.of(context).popUntil((Route<dynamic> route) => route.isFirst);
+      },
+    );
     return Scaffold(
       appBar: AppBar(
         title: Text(toString()),
