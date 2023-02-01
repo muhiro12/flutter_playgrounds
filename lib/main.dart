6029b3d63c850bd872831b7fa820635792b6b599
muhiro_ <muhiro.12@gmail.com>
2023-02-02 02:19:21 +0900

    Update package "Riverpod"

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_playgrounds/app.dart';
-import 'package:flutter_playgrounds/business/model/git_hub.dart';
-import 'package:flutter_playgrounds/data/entity/gitignore.dart';
-import 'package:flutter_playgrounds/data/model/preferences.dart';
-import 'package:flutter_playgrounds/data/repository/fake/fake_git_hub_repository.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import 'app.dart';
+import 'business/model/git_hub.dart';
+import 'data/model/preferences.dart';
+import 'data/repository/fake/fake_git_hub_repository.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,14 +14,12 @@ Future<void> main() async {
   runApp(
     ProviderScope(
       overrides: <Override>[
-        preferencesProvider.overrideWithValue(
-          Preferences(sharedPreferences),
+        preferencesProvider.overrideWith(
+          (_) => Preferences(sharedPreferences),
         ),
         // TODO(nakano): Not working yet
-        gitignoreProvider('').overrideWithValue(
-          AsyncValue<Gitignore>.data(
-            await FakeGitHubRepository().gitignore(''),
-          ),
+        gitignoreProvider('').overrideWith(
+          (_) => FakeGitHubRepository().gitignore(''),
         ),
       ],
       child: const App(),
